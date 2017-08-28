82c7ab21c7f201e4b95e85996723b3e47c988eac
安達和史 <kazu40808@gmail.com>
2017-08-28 12:29:00 +0900

    add redirect_to index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,11 +9,13 @@ class TweetsController < ApplicationController
 
   def create
     Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    redirect_to action: :index
   end
 
   def destroy
     tweet = Tweet.find(params[:id])
     tweet.destroy if current_user.id == tweet.user_id
+    redirect_to action: :index
   end
 
   def edit
@@ -23,6 +25,7 @@ class TweetsController < ApplicationController
   def update
     tweet = Tweet.find(params[:id])
     tweet.update(tweet_params) if current_user.id == tweet.user_id
+    redirect_to action: :index
   end
 
   def show
@@ -31,12 +34,9 @@ class TweetsController < ApplicationController
   end
 
   private
+
   def tweet_params
     params.permit(:image, :text)
-  end
-
-  def move_to_index
-    redirect_to action: :index
   end
 
 end
