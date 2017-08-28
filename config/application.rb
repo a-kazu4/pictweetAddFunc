08b07702fedb9ebd5c04738b4c56cea9db76122c
安達和史 <kazu40808@gmail.com>
2017-08-28 11:27:29 +0900

    origin pictweet

---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Pictweet2
+  class Application < Rails::Application
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
