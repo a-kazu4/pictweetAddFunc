89f7e91d96223d518db60587430bddb5c9907eae
安達和史 <kazu40808@gmail.com>
2017-08-28 14:46:37 +0900

    add post routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'home' => 'home#index'
+  post 'home' => 'home#post'
+
   resources :tweets do
     resources :comments, only:[:create]
   end
