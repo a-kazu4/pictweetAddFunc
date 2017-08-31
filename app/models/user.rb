8ee136290dab544e6ee89c0332a0596dc5467646
安達和史 <kazu40808@gmail.com>
2017-08-31 16:39:31 +0900

    adjust indents

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
+
   has_many :tweets
+
   has_many :comments
+
 end
