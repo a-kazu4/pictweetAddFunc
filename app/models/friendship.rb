eb4be37dd132cdbbc457d2707bc288db6a33acff
安達和史 <kazu40808@gmail.com>
2017-08-31 16:40:46 +0900

    belongs_to :following

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,5 @@
 class Friendship < ActiveRecord::Base
+  belongs_to :following, class_name:  'User',
+                         foreign_key: 'followee_id'
+  validates :followee_id, presence: true
 end
