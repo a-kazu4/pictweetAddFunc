6d9dcabac6bf55f8a08d2bbabe202e28e18eafd4
安達和史 <kazu40808@gmail.com>
2017-08-31 16:42:17 +0900

    add @followings and @followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(params[:id])
-    @nickname = current_user.nickname
-    @tweets = user.tweets.page(params[:page]).per(5).order('created_at DESC')
+    @user = User.find(params[:id])
+    @tweets = @user.tweets.page(params[:page]).per(5).order('created_at DESC')
+    @followings = @user.followings
+    @followers = @user.followers
   end
 
 end
