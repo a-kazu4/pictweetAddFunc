10de4c6852b2f41e3d01d8abceac9b83815551e5
安達和史 <kazu40808@gmail.com>
2017-09-02 12:53:06 +0900

    set routing for following and followers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'home#index'
+
   get 'home' => 'home#index'
   post 'home' => 'home#post'
 
@@ -9,7 +11,12 @@ Rails.application.routes.draw do
   resources :tweets do
     resources :comments, only:[:create]
   end
-  resources :users, only:[:show]
+
+  resources :users, only:[:show] do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :friendships, only:[:create, :destroy]
 end
